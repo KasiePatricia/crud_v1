ba39990e2a77188df2910e8fe4c8a4a97d676958
KasiePatricia <ugwukasiep@gmail.com>
2023-10-31 23:53:11 +0100

    bug fix

--- app/__test__/auth.test.js
+++ app/__test__/auth.test.js
@@ -8,17 +8,39 @@ const axios = require("axios");
   npm run text
 */
 
+const registerBody = {
+  fullName: "moon Doe",
+  userName: "fantaice",
+  password: "fantaice",
+  role: "admin",
+}
+
+const loginBody =  {
+  userName: "fantaice",
+  password: "fantaice",
+}
+
+const itemBody =  {
+  itemsName: "fan",
+  description: "Buy this fan",
+  price: 8000,
+  isInStock: true,
+}
+
+let token;
+let itemId;
+
 const Authorization =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2NTQwMzIwZjE1NWZiOTM2Yzg0Mjk5YTMiLCJ1c2VyTmFtZSI6ImhlbnJ5MTIzIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjk4NzUyNjM0fQ.ayHUpZDEKJkmBVLf1ZmNMqpuYDVbDUyPZ3CuhIai4Ok";
 
 // register
-test("Test to see if the registration works well", async () => {
+test("for successful registration", async () => {
   try {
-    const response = await axios.post("http://localhost:4000/auth/register", {
-      fullName: "moon Doe",
-      userName: "jayzeme",
-      password: "jayzeme",
-      role: "admin",
+    const response = await axios.post("http://localhost:4000/auth/register", registerBody,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     expect(response.status).toBe(201);
@@ -29,12 +51,16 @@ test("Test to see if the registration works well", async () => {
 });
 
 // login
-test("Test to login a user/admin", async () => {
+test("for successful login", async () => {
   try {
-    const response = await axios.post("http://localhost:4000/auth/login", {
-      userName: "jayzeme",
-      password: "jayzeme",
+    const response = await axios.post("http://localhost:4000/auth/login", loginBody, 
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    token = response.body.token;
 
     expect(response.status).toBe(200);
     expect(response.data.success).toBe(true);
@@ -44,23 +70,23 @@ test("Test to login a user/admin", async () => {
 });
 
 // Add a item
-test("Add a item", async () => {
+test("for adding an item to your cart", async () => {
   try {
+    if (!token) {
+      throw new Error("Token is not available from the login test");
+    }
+
     const response = await axios.post(
-      "http://localhost:4000/shop/add-item",
-      {
-        itemsName: "Yam",
-        description: "Buy this yam",
-        price: 800,
-        isInStock: true,
-      },
+      "http://localhost:4000/shop/add-item", itemBody,
       {
         headers: {
-          Authorization,
+          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
       }
     );
+
+    itemId = response.body.data?.item._id;
 
     expect(response.status).toBe(201);
     expect(response.data.success).toBe(true);
@@ -69,11 +95,15 @@ test("Add a item", async () => {
 });
 
 // Get all items
-test(" Get all items", async () => {
+test("for getting all items", async () => {
   try {
+    if (!token) {
+      throw new Error("Token is not available from the login test");
+    }
+
     const response = await axios.get("http://localhost:4000/shop/items", {
       headers: {
-        Authorization,
+        Authorization: `Bearer ${token}`,
       },
     });
 
@@ -83,14 +113,24 @@ test(" Get all items", async () => {
 });
 
 // Delete an item
-test(" Delete an items", async () => {
+test("for successful deletion", async () => {
   try {
+    if (!token) {
+      throw new Error("Token is not available from the login test");
+    }
+
+    if (!itemId) {
+      throw new Error(
+        "ItemId is not available from the shopitem creation test"
+      );
+    }
+
     const response = await axios.delete(
-      "http://localhost:4000/shop/item/6540ebcada7da8aaf25355ab",
+      `http://localhost:4000/shop/item/${itemId}`,
 
       {
         headers: {
-          Authorization,
+          Authorization: `Bearer ${token}`,
         },
       }
     );
